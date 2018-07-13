9155aa025490b9af15aa90b6e755c422f2db762f
Ethan Knox <norton120@gmail.com>
2018-07-13 13:54:39 -0400

    Audit (#1)
    * moved audit partials to audit
    
    * added indentifier and schema quoting, fixed macro open end
    
    * comments, working incremental audit base
    
    * elaborated on screen template
    
    * beefed up kdbt_gen for screen
    
    * added screen aggregating model
    
    * added error_event_fact to standup

---
+++ audits/audit.sql
@@ -0,0 +1,45 @@
+---------- INITIAL AUDIT 
+---- All the initial audit runs are performed here for a build.
+---- These runs are only to set the min and max values for the audit 
+---- and to populate the AUDIT table with a working row to update
+---- as the screens complete. 
+
+---------- FORMATTING
+---- To help keep this from becoming a mess, follow these rules: 
+---- * 3 newlines between CTEs
+---- * Keep CTEs in alphabetical order. Yes it makes git diffs harder to read.
+
+
+---------- CONFIGURATION
+    {{config({
+        "materialized":"incremental",
+        "sql_where":"TRUE",
+        "schema":"QUALITY",
+        "post-hook":[
+            "{{comment({'column':'audit_key','definition':'The supernatural key for the audit table.'})}}",
+            "{{comment({'definition':'Every time we execute a quality process job against new data in the Data Lake the instance of that execution is called an audit.', 
+                        'grain':'One row per audit executed on a unique entity'})}}"    
+    
+   ]})}}
+
+
+---- Set already exists flag 
+    {% set audit_exists = adapter.already_exists(this.schema,this.name) %}
+
+WITH
+
+---- Each macro is a self-contained CTE. Just add vars and post-commas.
+
+---- example:
+----   initial_audit_partial("YOUR_SCHEMA", "YOUR_TABLE","YOUR_COLUMN")  
+
+
+    {{initial_audit_partial("ERP", "ORDERS","_METADATA__TIMESTAMP","TIMESTAMP_NTZ")}} 
+
+SELECT
+    *
+FROM
+    erp_orders_new_audit_record
+
+
+    
--- macros/kdbt_utils/initial_audit_partial.sql
+++ macros/kdbt_utils/initial_audit_partial.sql
@@ -1,21 +1,20 @@
-{%- macro initial_audit_partial(schema_key, entity_key, cdc_target, cast_target_as= False, incremental = True, entity_type = 'Table', database_key = 'RAW') -%}
-/*  INTENT: gets the CDC range values for this audit, builds an 
-    initial row without measures that we set to 'In Progress' status.
-
-    ARGS:
-        - schema_name (string) the raw source schema.
-        - cdc_target (string) the name of the column that indicates a new or updated row.
-        - cast_target_as (varchar default None) if not none, cast the target before comparison.
-        - incremental (boolean default True) does the audit table already exist.
-        - entity_key (string) the name of the source entity.
-        - entity_type (string default 'Table') the type of source entity targeted.
-        - database_name (string default 'RAW') the raw source database.
-    RETURNS: string fully wrapped CTE named '<schema>_<table>_initial_audit'
-*/
+{%- macro initial_audit_partial(schema_key, entity_key, cdc_target, cast_target_as= 'TIMESTAMP', incremental = True, entity_type = 'Table', database_key = 'RAW') -%}
+----  INTENT: gets the CDC range values for this audit, builds an 
+----    initial row without measures that we set to 'In Progress' status.
+----
+----    ARGS:
+----       - schema_name (string) the raw source schema.
+----       - cdc_target (string) the name of the column that indicates a new or updated row.
+----      - cast_target_as (varchar default TIMESTAMP) cast the target before comparison.
+----       - incremental (boolean default True) does the audit table already exist.
+----       - entity_key (string) the name of the source entity.
+----       - entity_type (string default 'Table') the type of source entity targeted.
+----       - database_name (string default 'RAW') the raw source database.
+----   RETURNS: string fully wrapped CTE named '<schema>_<table>_initial_audit'
 
 {{schema_key}}_{{entity_key}}_new_audit_record AS (      
     -- get the max value of the most recent audit if the audit table exists
-    {%- if incremental -%}
+    {% if incremental %}
     WITH
     {{schema_key}}_{{entity_key}}_audit_max AS (
         SELECT
@@ -23,44 +22,42 @@
         FROM
            "{{target.database}}"."QUALITY"."AUDIT"
         WHERE
-            database_key = "{{database_name}}"
+            database_key = '{{database_key}}'
         AND
-            schema_key = "{{schema_key}}"
+            schema_key = '{{schema_key}}'
         AND
-            entity_key = "{{entity_key}}"
+            entity_key = '{{entity_key}}'
         AND
-            entity_type = "{{entity_type}}"
+            entity_type = '{{entity_type}}'
     ),
 
     {%- endif -%}
 
 ---- get min and max value of source for this audit
-    {{schema_name}}_{{target_name}}_source_min_max AS (
+    {{schema_key}}_{{entity_key}}_source_min_max AS (
         SELECT
             MIN({{cdc_target}}) AS lowest_cdc,
             MAX({{cdc_target}}) AS highest_cdc
         FROM
-            "{{database_name}}"."{{schema_name}}"."{{entity_name}}"
+            "{{database_key}}"."{{schema_key}}"."{{entity_key}}"
 
-    {%- if incremental -%}
+    {% if incremental %}
         WHERE
-            {%- if cast_target_as is not None -%}
-                {{cdc_target}}::{{cast_target_as}} > (SELECT record_max::{{cast_target_as}}  FROM {{schema_name}}_{{target_name}}_audit_max LIMIT 1)
-            {%- else -%}
-                {{cdc_target}} > (SELECT record_max FROM {{schema_name}}_{{target_name}}_audit_max LIMIT 1)
-            {%- endif -%}
+            {{cdc_target}}::{{cast_target_as}} > 
+                COALESCE((SELECT record_max::{{cast_target_as}}  FROM {{schema_key}}_{{entity_key}}_audit_max LIMIT 1),'1970-01-01 00:00:00'::timestamp)
 
     {%- endif -%}
-    ),
+    )
     
     -- create the final partial    
     SELECT
+        sequence.nextval AS audit_key,
         'In Process' AS audit_status,
-        {{cdc_target}} as cdc_target,
-        {{entity_type}} AS entity_type,
-        {{entity_key}} AS entity_key,
-        {{schema_key}} AS schema_key,
-        {{database_key}} AS database_Key,
+        '{{cdc_target}}' as cdc_target,
+        '{{entity_type}}' AS entity_type,
+        '{{entity_key}}' AS entity_key,
+        '{{schema_key}}' AS schema_key,
+        '{{database_key}}' AS database_Key,
         'Not Available' AS dbt_version,
         'Not Available' AS dbt_repo_release_version,
         {{schema_key}}_{{entity_key}}_source_min_max.lowest_cdc,
@@ -68,5 +65,7 @@
 
     FROM 
     
-    {{schema_name}}_{{target_name}}_source_min_max 
-)    
+    {{schema_key}}_{{entity_key}}_source_min_max,
+    TABLE(getnextval(quality_audit_pk_seq)) sequence  
+)
+
