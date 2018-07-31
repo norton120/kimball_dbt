aaf76f43be2827178e1ef4d48e514ead653ff42e
ssaladino19 <steven.saladino@revzilla.com>
2018-07-31 14:13:13 -0400

    Change provided_value from optional to required.

--- macros/screens/values_at_least.sql
+++ macros/screens/values_at_least.sql
@@ -1,20 +1,19 @@
 ---------- values_at_least SCREEN
 ---- Verifies that each row in a column is greater than or equal a provided value, if not null.
 
----------- Signature
-----
-----
-----
-
-
 {%- macro null_screen(screen_args, kwargs) -%}
-    {{kwargs.database}}_{{kwargs.schema}}_{{kwargs.entity}}_{{screen_args.column}}_VALUES_AT_LEAST_{{kwargs.provided_value}} AS (
+---- Pass the screen_args object with these params:
+---- screen_args:
+----    - blacklisted_values (list) the values to deny
+----    - value_type (string) the datatype for the list of values
+
+    {{kwargs.database}}_{{kwargs.schema}}_{{kwargs.entity}}_{{screen_args.column}}_VALUES_AT_LEAST_{{screen_args.provided_value}} AS (
         SELECT
-            {{universal_audit_property_set('VALUES_AT_LEAST_{{kwargs.provided_value}}',screen_args,kwargs)}}
+            {{universal_audit_property_set('values_at_least_',screen_args,kwargs)}}
 
         AND
             (
-                    {{screen_args.column}} >= {{kwargs.provided_value}}
+                    {{screen_args.column}} >= {{screen_args.provided_value}}
                 OR
                     {{screen_args.column}} IS NULL
             )
