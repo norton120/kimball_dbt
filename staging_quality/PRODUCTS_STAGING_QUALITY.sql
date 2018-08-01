94719a4d55e23a758469396024ad19cd220c6459
ssaladino19 <steven.saladino@revzilla.com>
2018-08-01 10:35:43 -0400

    Fixed staging quality template to skip audit_key NOT IN condition, depending on Jinja2 if statement, and deleted insidious record_identifier reference.

--- staging_quality/PRODUCTS_STAGING_QUALITY.sql
+++ staging_quality/PRODUCTS_STAGING_QUALITY.sql
@@ -1,8 +1,8 @@
 ---------- PRODUCTS_STAGING_QUALITY TABLE
 ----
----- Staging Quality tables are cleaned and transform-ready source tables.     
----- The data in Staging Quality tables are completely untransformed source data, with one exception: they 
----- have additional attributes AUDIT_KEY, ROW_QUALITY_SCORE and AUDIT_QUALITY_SCORE. 
+---- Staging Quality tables are cleaned and transform-ready source tables.
+---- The data in Staging Quality tables are completely untransformed source data, with one exception: they
+---- have additional attributes AUDIT_KEY, ROW_QUALITY_SCORE and AUDIT_QUALITY_SCORE.
 ----
 ---- AUDIT_KEY is the FKey to the audit that added (or last updated) the subject row.
 ----
@@ -41,15 +41,15 @@
             (SELECT
                 data_type
             FROM
-                "RAW".information_schema.columns 
+                "RAW".information_schema.columns
             WHERE
                 table_schema = 'ERP'
             AND
                 table_name = 'PRODUCTS'
             AND
-                record_identifier.column_name = UPPER('id')
+                column_name = UPPER('id')
             LIMIT 1) record_identifier
-        ON 1=1 
+        ON 1=1
 
         WHERE
             audit_status = 'Completed'
@@ -59,13 +59,14 @@
             schema_key = 'ERP'
         AND
             entity_key = 'PRODUCTS'
-
+        {% if adapter.already_exists(this.schema,this.name) %}
         AND
             audit_key NOT IN (SELECT
                                 DISTINCT audit_key
                               FROM
                                 {{this}})
-        ORDER BY audit_key DESC 
+        {% endif %}
+        ORDER BY audit_key DESC
         LIMIT 1
 
     {%- endcall -%}
@@ -80,21 +81,21 @@
                             'cdc_target' : audit_response[0][1],
                             'lowest_cdc' : audit_response[0][2],
                             'highest_cdc' : audit_response[0][3],
-                            'cdc_data_type' : audit_response[0][4], 
+                            'cdc_data_type' : audit_response[0][4],
                             'record_identifier_data_type' : audit_response[0][5]} -%}
 
     WITH
     audit_source_records AS (
 
-        SELECT 
+        SELECT
             *,
             {{audit_data['audit_key']}} AS audit_key
         FROM
-           RAW.ERP.PRODUCTS 
+           RAW.ERP.PRODUCTS
         WHERE
             {{audit_data['cdc_target']}}
         BETWEEN
-        
+
         {% if audit_data['cdc_data_type'] in ('TEXT','TIMESTAMP_NTZ') %}
             '{{audit_data["lowest_cdc"]}}' AND '{{audit_data["highest_cdc"]}}'
         {% else %}
@@ -108,7 +109,7 @@
 
             -- for audit-level error events this will be NULL so we will remove them later
             -- and use the presence of NULL values to flag an audit-level event
-            TRY_CAST(record_identifier AS {{audit_data['record_identifier_data_type']}}) AS id           
+            TRY_CAST(record_identifier AS {{audit_data['record_identifier_data_type']}}) AS id
         FROM
             {{this.database}}.{{this.schema | replace('STAGING_QUALITY','QUALITY')}}.error_event_fact
         WHERE
@@ -118,29 +119,29 @@
 
 
 
----- remove rejected rows, flag flagged rows, and add audit-level flag 
-    SELECT 
-        audit_source_records.*, 
+---- remove rejected rows, flag flagged rows, and add audit-level flag
+    SELECT
+        audit_source_records.*,
 
         CASE
             WHEN error_event_action IS NULL THEN 'Passed'
             ELSE error_event_action
         END AS row_quality_score,
-        
+
         (SELECT
             CASE
-                WHEN COUNT(*) > 0 THEN 'Flagged' 
+                WHEN COUNT(*) > 0 THEN 'Flagged'
                 ELSE 'Passed'
             END
         FROM
             error_events
-        WHERE 
-            id IS NULL) AS audit_quality_score    
+        WHERE
+            id IS NULL) AS audit_quality_score
     FROM
         audit_source_records
     LEFT JOIN
-        error_events 
-    ON 
+        error_events
+    ON
         audit_source_records.id = error_events.id
 
     WHERE
@@ -152,19 +153,19 @@
         SELECT
             *
         FROM
-            {{this}}       
+            {{this}}
         WHERE 1=0
 {% else %}
         SELECT
             *,
-            0::integer AS audit_key,    
+            0::integer AS audit_key,
             ''::varchar AS row_quality_score,
             ''::varchar AS audit_quality_score
         FROM
             RAW.ERP.PRODUCTS
         WHERE
-            0=1    
-{% endif %} 
+            0=1
+{% endif %}
 
 ---------- CONFIGURATION [leave this section alone!]
 {{config({
@@ -177,4 +178,4 @@
 
 
 ---------- DEPENDENCY HACK
----- {{ref('AUDIT_FACT')}}   
+---- {{ref('AUDIT_FACT')}}
