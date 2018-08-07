3106d0212e003a3286bb45ddc8ea35b950cea7a3
Ethan Knox <norton120@gmail.com>
2018-08-07 14:28:22 -0400

    cleaning up macros and comments

--- macros/print_columns.sql
+++ macros/print_columns.sql
@@ -1,9 +1,11 @@
----------- PRINT COLUMNS
 {% macro print_columns(columns, qualifier=None) %}
----- prints out columns in comma seperated list
+{#
+---- INTENT: prints out columns in comma seperated list
 ---- ARGS:
 ----    - columns (list) the list of columns to itemize
 ----    - qualifier(string) the entity qualifier. default is no qualifier.
+---- RETURNS: string of printed column entries. Note that last entry does not get a comma.
+#}
     {% for col in columns %}
        {{qualifier+'.' if qualifier != None else ''}}{{col}}{{',' if not loop.last}}
     {% endfor %}
