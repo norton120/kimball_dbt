84a9d82883f658203ed382518527458cc856db4e
ssaladino19 <steven.saladino@revzilla.com>
2018-08-14 12:37:26 -0400

    Add conditions for Fkey, in case schema fkey_entity fails already_exists

--- macros/post_hooks/add_constraints.sql
+++ macros/post_hooks/add_constraints.sql
@@ -1,22 +1,26 @@
 {% macro add_constraints(constraints, schema, entity, attribute, fkey_entity = None, fkey_attribute = None) %}
 {#---- INTENT: creates DDL constraint strings for use in post-hooks
 ---- ARGS:
-----    - constraints (list) a list of constraints to apply. Options are Pkey, FKey, Unique 
+----    - constraints (list) a list of constraints to apply. Options are Pkey, FKey, Unique
 ----    - attribute (string) the name of the column to apply the constraint against.
 ----    - entity (string) the fully qualified entity path
 ----    - fkey_entity (string) the entity name to fkey against
 ----    - fkey_attribute (string) the attribute to fkey against
----- RETURNS: string the compiled DDL statement 
+---- RETURNS: string the compiled DDL statement
 ---- Note: this sets the constraints on __dbt_tmp table which is then renamed into the prod table
 
 #}
     {% for con in constraints %}
-        ALTER TABLE {{schema}}.{{entity}}__dbt_tmp 
+        ALTER TABLE {{schema}}.{{entity}}__dbt_tmp
         {% if con == 'Null' %}
             ALTER COLUMN {{attribute}} NOT NULL
         {% elif con == 'Fkey' %}
-            ADD CONSTRAINT {{con}}_{{attribute}} 
-            FOREIGN KEY ({{attribute}}) REFERENCES {{fkey_entity}} ({{fkey_attribute}})
+            {% if adapter.already_exists(schema, fkey_entity) %}
+                ADD CONSTRAINT {{con}}_{{attribute}}
+                FOREIGN KEY ({{attribute}}) REFERENCES {{fkey_entity}} ({{fkey_attribute}})
+            {% else %}
+                UNSET DATA_RETENTION_TIME_IN_DAYS
+            {% endif %}
         {% elif con == 'Pkey' %}
             ADD CONSTRAINT {{con}}_{{attribute}}
             PRIMARY KEY ({{attribute}})
@@ -27,5 +31,3 @@
     {% endfor %}
 
 {% endmacro %}
-        
-            
